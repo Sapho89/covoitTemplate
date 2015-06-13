4f1cf2538a6ea0618c34b890050be0b5a36e6264
Cecile BOURGUIGNON <sapho89@example.com>
2015-06-13 12:55:14 +0200

    espace personnel

---
+++ footer.php
@@ -0,0 +1,81 @@
+
+	<!-- Footer -->
+		<footer id="footer">
+			<div class="bottom-footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 text-center">
+                                                    						
+							<div class="footer-about-us">
+								<div class="social-icons">
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-google-plus"></i></a>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+									<a href="#"><i class="fa fa-pinterest-p"></i></a>
+									<a href="#"><i class="fa fa-instagram"></i></a>
+								</div>
+							</div>
+							<div class="copyright">
+								©2015 Axes Theme  |  Designed by <a href="http://deothemes.com/">DeoThemes</a>
+							</div>
+						</div> <!-- end col -->
+					</div> <!-- end row -->
+				</div> <!-- end container -->
+			</div> <!-- end bottom footer -->
+		</footer> <!-- end footer -->
+
+		<div id="back-to-top">
+			<a href="#top"><i class="fa fa-angle-up"></i></a>
+		</div>
+
+	</div> <!-- end main-wrapper -->
+	
+
+	<!-- jQuery Scripts -->
+	<script src="js/jquery.min.js"></script>
+ 	<script type="text/javascript" src="js/lightbox.min.js"></script>	
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap-hover-dropdown.min.js"></script>
+	<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="js/jquery.countTo.js"></script>
+	<script type="text/javascript" src="js/jquery.appear.js"></script>
+	<script type="text/javascript" src="js/plugins-scroll.js"></script>
+	<script type="text/javascript" src="js/isotope.pkgd.min.js"></script>
+	<script type="text/javascript" src="js/jquery.easing.min.js"></script>
+	<script type="text/javascript" src="js/jquery.easypiechart.min.js"></script>
+	<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+	<script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>
+	<script src="js/wow.min.js"></script>
+
+	<!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
+	<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+	<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+	<!-- Custom Theme JavaScript -->
+    <script src="js/scripts.js"></script>
+
+	<!-- Revolution Slider -->
+	<script type="text/javascript">
+				jQuery(document).ready(function() {
+					   jQuery('.tp-banner').revolution(
+						{
+							delay:9000,
+							startwidth:1170,
+							startheight:680,
+							hideThumbs:10,
+
+							navigationType:"bullet",							
+							navigationStyle:"preview1",
+
+							hideArrowsOnMobile:"on",
+							
+							touchenabled:"on",
+							onHoverStop:"on",
+							spinner:"spinner4"
+						});
+				});
+	</script>
+	  
+</body>
+</html>
